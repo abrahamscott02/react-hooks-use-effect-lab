4b73506cfe8b56d905bd23827a0ad0554d2f30fc
Abraham Scott <abrahamscott02@gmail.com>
2024-05-02 13:39:44 -0600

    Done

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return
+    }
+
+    const timer = setTimeout(() => {
+      setTimeRemaining((prevTime) => {
+        return prevTime - 1;
+      });
+    }, 1000);
+
+    // Cleanup function to clear the timeout
+    return () => clearTimeout(timer);
+  }, [timeRemaining, onAnswered]); // Include onAnswered in the dependency array
+
+  const { id, prompt, answers, correctIndex } = question;
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
-
-  const { id, prompt, answers, correctIndex } = question;
 
   return (
     <>
